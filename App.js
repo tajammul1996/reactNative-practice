c19931b13b43d608caa3ad1cb6de3ec74a155ea7
Tajammul <tpzuffari@gmail.com>
2018-03-23 12:43:28 +0530

    Added styles to form

--- App.js
+++ App.js
@@ -5,27 +5,29 @@ import {
   Text,
   View,
   TextInput,
-  Button
+  Button,
+  TouchableOpacity
 } from 'react-native';
 
 export default class App extends Component{
 
-  changeName = (event) => {
-    alert(event);
-    console.log(event.target)
-  }
+  // changeName = (event) => {
+  //   alert(event);
+  //   console.log(event.target)
+  // }
 
   render() {
     return (
       <View style={styles.container}>
-      <Text>Register</Text>
+      <Text style={styles.headingStyle}>Register</Text>
         <View style= {styles.inputContainer}>
-          <TextInput placeholder="Full name" style={styles.input} onChangeText={this.changeName} />
+          <TextInput placeholder="Full name" style={styles.input} underlineColorAndroid="transparent"/>
           <TextInput placeholder="Email" style={styles.input}/>
-          <TextInput placeholder="Password" style={styles.input}/>
-          <Button title="Lets go"/>
-        </View>
-        
+          <TextInput placeholder="Password" style={styles.input} secureTextEntry />
+        </View>       
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.textStyle}>Lets go</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -42,6 +44,33 @@ const styles = StyleSheet.create({
   },
   input : {
     width: '100%',
-    height: 40
+    height: 40,
+    borderColor: 'steelblue',
+    borderWidth: 1,
+    padding: 10,
+    margin: 5,
+    borderRadius: 15,
+    textAlign: 'center'
+  },
+  buttonContainer: {
+
+    backgroundColor: '#d6d7da',
+    padding:5,
+    borderColor: 'powderblue',
+    borderWidth: 1,
+    width: '60%',
+    borderRadius: 10,
+    margin: 10,
+    height: 35
+  },
+  textStyle:{
+    textAlign: 'center',
+  },
+  headingStyle: {
+    fontFamily: 'helvetica',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'steelblue',
+    padding: 5
   }
 });
